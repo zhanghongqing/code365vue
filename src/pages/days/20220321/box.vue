7e8101b7b5edde5c6c008bcbb33f32b599b7b000
zhanghongqing <8366783@qq.com>
2022-03-21 22:19:51 +0800

    box

---
+++ src/pages/days/20220321/box.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Pos {
+  x: number
+  y: number
+}
+
+interface Props {
+  pos: Pos
+}
+
+withDefaults(defineProps<Props>(), {
+  pos: () => {
+    return { x: 1, y: 1 }
+  },
+})
+
+</script>
+
+<template>
+  <div w0 h0 bg-red relative preserve-3d class="box" :style="{'transform': `translateX(${100 * pos.x + 50}px) translateZ(50px) translateY(${100 * pos.y + 50}px)`}">
+    <div preserve-3d class="face top" w100px h100px bg-red op50 absolute />
+    <div preserve-3d class="face bottom" w100px h100px bg-blue op50 absolute />
+    <div preserve-3d class="face left" w100px h100px bg-green op50 absolute />
+    <div preserve-3d class="face right" w100px h100px bg-yellow op50 absolute />
+    <div preserve-3d class="face front" w100px h100px bg-pink op50 absolute />
+    <div preserve-3d class="face back" w100px h100px bg-purple op50 absolute />
+  </div>
+</template>
+
+<style>
+.box {
+  /* transform: translateZ(-100px); */
+  transform: translateX(50px) translateZ(50px) translateY(50px);
+}
+.face {
+  top: -50px;
+  left: -50px;
+  transform-origin: center center;
+  /* transform: matrix3d(
+    1,0,0,0,
+    0,1,0,0,
+    0,0,1,0,
+    0,0,0,1
+  ); */
+}
+.face.top {
+  transform: translateZ(50px) rotate(0deg);
+}
+.face.bottom {
+  transform: translateZ(-50px) rotate(0deg);
+}
+.face.left {
+  transform: translateX(-50px) rotateY(90deg);
+}
+.face.right {
+  transform: translateX(50px) rotateY(90deg);
+}
+.face.front {
+  transform: translateY(50px) rotateX(90deg);
+}
+.face.back {
+  transform: translateY(-50px) rotateX(90deg);
+}
+</style>
---
+++ src/pages/days/20220321/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import Box from './box.vue'
+
+interface Pos {
+  x: number
+  y: number
+}
+
+const data = reactive({
+  pos: Array<Pos>(),
+})
+
+const { pos } = toRefs(data)
+
+const initPos = () => {
+  console.log('初始化')
+  while (pos.value.length < 30) {
+    const x = Math.floor(Math.random() * 6)
+    const y = Math.floor(Math.random() * 6)
+    pos.value.push({ x, y })
+  }
+}
+
+onMounted(() => {
+  initPos()
+})
+</script>
+
+<template>
+  <div w100vw flex justify-center pt100px preserve-3d class="stage">
+    <div w-600px h-600px bg-gray preserve-3d class="floor">
+      <Box v-for="(p, index) in pos" :key="index" :pos="p" />
+    </div>
+  </div>
+</template>
+
+<style>
+.stage {
+  perspective: 1000px;
+}
+.floor {
+  transform: rotateX(40deg) rotateZ(40deg);
+}
+</style>
